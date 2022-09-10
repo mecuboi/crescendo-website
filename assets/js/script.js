261363146b5aa566a532014613221abb636fc4b5
mecuboi <matthewd.jusuf@gmail.com>
2022-09-11 00:45:54 +1000

    adding logic to local storage and also displaying a maximum of 5 recent searches and putting the recent one on top

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var searchResultsEl = $("#search-results");
 var searchButtonEl = $("#search-button");
 var inputEl = $("#search-input");
+var searchList = [];
+var searchListContainer = $('#search-list-container')
+
 
 // Never push api key to GitHub
 var apiKey = "AIzaSyD9C2bxQ7DkETOL3OhnUY9n9ckg6UlOI1I";
@@ -9,28 +12,66 @@ var apiEndpoint = `https://www.googleapis.com/youtube/v3/search?part=snippet&max
 
 function searchVideos(event) {
     event.preventDefault();
-    storeSearch();
+    console.log(searchString)
     console.log('success')
+
     var searchString = $(inputEl).val().trim();
-    if (searchString) {
-      fetch(apiEndpoint + searchString)
-        .then(
-          function (response) {
-            return response.json();
-          },
-          function (error) {
-            console.log(error.message);
-          }
-        )
-        .then(function (data) {
-          renderVideos(data);
-        });
+    searchList.push(searchString);
+    inputEl.val('');
+    
+    storeSearchList();
+    renderSearchList();
+    
+    // if (searchString) {
+    //   fetch(apiEndpoint + searchString)
+    //     .then(
+    //       function (response) {
+    //         return response.json();
+    //       },
+    //       function (error) {
+    //         console.log(error.message);
+    //       }
+    //     )
+    //     .then(function (data) {
+    //       renderVideos(data);
+    //     });
+    // }
+  }
+
+// This function is being called below and will run when the page loads.
+function init() {
+    // Get stored todos from localStorage
+    var recentSearchList = JSON.parse(localStorage.getItem("recentSearch"));
+    if (recentSearchList !== null) {
+      searchList = recentSearchList;
     }
+    renderSearchList();
   }
-
-  function storeSearch() {
-    localStorage.setItem('recentSearch',inputEl.val().trim() )
-    console.log(inputEl.val().trim())
+  
+  function storeSearchList() {
+    // Stringify and set key in localStorage to todos array
+    localStorage.setItem("recentSearch", JSON.stringify(searchList));
   }
+  
+function renderSearchList() {
+    searchListContainer.html('');
 
-  $(searchButtonEl).on("click", searchVideos);
+    for (var i = 0; i < searchList.length && i < 5; i++) {
+        searchList.reverse();
+
+        var search = searchList[i];
+
+        var recentSearchButton = $('<button class="recent-search bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full">')
+
+        recentSearchButton
+            .text(search)
+            .appendTo(searchListContainer);
+
+    }
+}
+ 
+  
+
+searchButtonEl.on("click", searchVideos);
+
+init();
