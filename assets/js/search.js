ee8573222e06f93f94fc7de1640380256ff61e31
mecuboi <matthewd.jusuf@gmail.com>
2022-09-14 00:11:28 +1000

    removing button

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,18 +47,11 @@ function renderVideos(data) {
       `<a href="video.html?videoId=${videoId}" class=" grid grid-cols-12 col-span-12">`
     );
     var imageEl = $(
-      `<img class="col-span-12 w-full sm:col-span-6 md:col-span-3 justify-self-center p-1"/>`
+      `<img class="col-span-12 w-full sm:col-span-6 md:col-span-4 justify-self-center p-1"/>`
     );
     $(imageEl).attr("src", items[i].snippet.thumbnails.medium.url);
     var detailsEl = $(
-      '<div class="col-span-12 sm:col-span-5 md:col-span-7 p-1">'
-    );
-    var tinyUrlButton = $(
-      `<button 
-        class="tiny-url-button col-span-12 md:col-span-3 text-white bg-gradient-to-r from-blue-500 via-blue-600 
-        to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg 
-        shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-2 my-2"
-        type='button' data-modal-toggle='defaultModal'>`
+      '<div class="col-span-12 sm:col-span-5 md:col-span-8 p-1">'
     );
 
     var titleEl = $(`<h2 class="font-bold"><a href=${videoId}>`).text(
@@ -66,17 +59,12 @@ function renderVideos(data) {
     );
     var descriptionEl = $("<p>").text(items[i].snippet.description);
 
-    //DOM manipulation to insert the videos into our HTML page
-    tinyUrlButton.text('Generate Tiny URL');
     $(detailsEl).append(titleEl);
     $(detailsEl).append(descriptionEl);
     $(containerEl).append(imageEl);
     $(containerEl).append(detailsEl);
-    $(masterContainer).append(containerEl);
-    $(masterContainer).append(tinyUrlButton);
-    searchResultsEl.append(masterContainer);
-    
-    
+    searchResultsEl.append(containerEl); 
+  
   }
 }
 
@@ -89,30 +77,5 @@ $(searchForm).on("submit", function (event) {
   }
 });
 
-
-// tinyUrlButton.on('click', function (event) {
-//   event.preventDefault();
-//   var token = "yj3Juk60zzXWwFP2p1nrSwNMzh780fUMLZZOSKijLoeseni9pqaEISjgL6KK";
-//   var link = $(videoUrlEl).val();
-
-//   var requestOptions = {
-//     method: "POST",
-//     redirect: "follow",
-//   };
-
-//   fetch(
-//     `https://api.tinyurl.com/create?url=${link}&api_token=${token}`,
-//     requestOptions
-//   )
-//     .then((response) => response.json())
-//     .then((result) => displayTinyUrl(result))
-//     .catch((error) => console.log("error", error));
-// })
-
-// function displayTinyUrl(data) {
-//   var tinyUrl = data.data.tiny_url;
-//   $(tinyUrlEl).text(tinyUrl);
-//   $(asideEl).append(tinyUrlEl);
-// }
 
 initialize();
