87c4c7b6ae1b99b2eaa112e121a7ee85e24ec79d
Anisha Sapkota <anisha.sapkota79@gmail.com>
2022-09-13 16:15:59 +1000

    code clean up

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,5 +1,5 @@
 var iframeEl = $("#iframe");
-var asideEl = $("#tinyurlAside")
+var asideEl = $("#tinyurlAside");
 var videoUrlEl = $("#videoUrl");
 var buttonEl = $("#tinyUrlBtn");
 var tinyUrlEl = $(
@@ -10,7 +10,6 @@ $(buttonEl).on("click", handleClick);
 
 function handleClick(event) {
   event.preventDefault();
-  console.log("handleClick");
   var token = "yj3Juk60zzXWwFP2p1nrSwNMzh780fUMLZZOSKijLoeseni9pqaEISjgL6KK";
   var link = $(videoUrlEl).val();
 
@@ -28,23 +27,20 @@ function handleClick(event) {
     .catch((error) => console.log("error", error));
 }
 
-function displayTinyUrl(data){
-    var tinyUrl = data.data.tiny_url
-    $(tinyUrlEl).text(tinyUrl);
-$(asideEl).append(tinyUrlEl);
-
+function displayTinyUrl(data) {
+  var tinyUrl = data.data.tiny_url;
+  $(tinyUrlEl).text(tinyUrl);
+  $(asideEl).append(tinyUrlEl);
 }
 
 function initialize() {
   var query = document.location.search.replace("?videoId=", "");
-  //console.log(document.location.search);
   if (query) {
     renderVideos(query);
   }
 }
 
 function renderVideos(query) {
-  //console.log(query);
   $(iframeEl).attr(
     "src",
     `https://www.youtube.com/embed/${query}?autoplay=1&mute=1`
